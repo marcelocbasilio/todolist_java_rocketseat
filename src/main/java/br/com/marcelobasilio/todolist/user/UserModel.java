f511832053897d7b33f09eb8f9ed51d16602bdb1
Marcelo Basílio <marcelocbasilio@gmail.com>
2023-10-11 03:02:17 -0300

    second commit: second class completed.

--- src/main/java/br/com/marcelobasilio/todolist/user/UserModel.java
+++ src/main/java/br/com/marcelobasilio/todolist/user/UserModel.java
@@ -1,8 +1,29 @@
 package br.com.marcelobasilio.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
     
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
